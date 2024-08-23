58297d16f85b2db41e62ffcd66886dcf8792028c
Saurabh Jain <saurabhjain.net@gmail.com>
2024-08-23 11:07:00 -0700

    Fetching the list response as per given instructions.

--- app/src/main/java/com/tps/challenge/features/storefeed/StoreFeedFragment.kt
+++ app/src/main/java/com/tps/challenge/features/storefeed/StoreFeedFragment.kt
@@ -1,15 +1,21 @@
 package com.tps.challenge.features.storefeed
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.tps.challenge.R
 import com.tps.challenge.TCApplication
+import com.tps.challenge.ViewModelFactory
+import com.tps.challenge.viewmodel.StoreFeedViewModel
+import javax.inject.Inject
 
 /**
  * Displays the list of Stores with its title, description and the cover image to the user.
@@ -21,6 +27,37 @@ class StoreFeedFragment : Fragment() {
     private lateinit var storeFeedAdapter: StoreFeedAdapter
     private lateinit var recyclerView : RecyclerView
     private lateinit var swipeRefreshLayout : SwipeRefreshLayout
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory<StoreFeedViewModel>
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onStart() {
+        super.onStart()
+
+        val viewModel: StoreFeedViewModel by lazy {
+            viewModelFactory.get<StoreFeedViewModel>(
+                requireActivity()
+            )
+        }
+
+        viewModel.storesData.observe(this, Observer {
+            if(0 < it?.size!!) {
+                storeFeedAdapter.stores = it
+                storeFeedAdapter.notifyDataSetChanged()
+                swipeRefreshLayout.isRefreshing = false
+            }
+        })
+        viewModel.fetchStoresData()
+
+        swipeRefreshLayout.setOnRefreshListener {
+            Log.i(TAG, "onRefresh called from SwipeRefreshLayout")
+
+            // This method performs the actual data-refresh operation and calls
+            // setRefreshing(false) when it finishes.
+            viewModel.fetchStoresData()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         TCApplication.getAppComponent().inject(this)
@@ -34,15 +71,14 @@ class StoreFeedFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_store_feed, container, false)
         swipeRefreshLayout = view.findViewById(R.id.swipe_container)
         // Enable if Swipe-To-Refresh functionality will be needed
-        swipeRefreshLayout.isEnabled = false
+        swipeRefreshLayout.isEnabled = true
 
         storeFeedAdapter = StoreFeedAdapter()
         recyclerView = view.findViewById(R.id.stores_view)
         recyclerView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(activity)
-            // TODO uncomment the line below whe Adapter is implemented
-            // adapter = storeFeedAdapter
+            adapter = storeFeedAdapter
         }
         return view
     }
